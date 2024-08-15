75418d2046d7f3ee84f9602b2e8c88603114d4d6
majid-the-dev <contactmajidoladepo@gmail.com>
2024-08-15 16:19:56 +0100

    commit message

--- components/Products.jsx
+++ components/Products.jsx
@@ -82,7 +82,7 @@ const Products = () => {
           <div className="relative w-full max-w-sm h-52 rounded-3xl overflow-hidden p-6 group">
             <Image
               src={"/assets/generator.png"}
-              alt="solar panel"
+              alt="solar inverters"
               layout="fill"
               objectFit="contain"
             />
